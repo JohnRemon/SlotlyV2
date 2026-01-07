85acef9cf38a184331d5870e098ab3b4b92260c1
JohnRemon <john1812008@gmail.com>
2026-01-08 00:11:58 +0100

    Added Password Complexity Checks

--- src/main/java/com/example/SlotlyV2/dto/RegisterRequest.java
+++ src/main/java/com/example/SlotlyV2/dto/RegisterRequest.java
@@ -21,6 +21,7 @@ public class RegisterRequest {
 
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password Size must be greater than 8 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&].*$", message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (@$!%*?&)")
     private String password;
 
     @NotBlank(message = "First name is required")
