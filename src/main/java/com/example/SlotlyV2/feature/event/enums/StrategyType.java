db7f8c9b9eb95c7024aa521f467213e0efc8b639
JohnRemon <john1812008@gmail.com>
2026-01-30 11:37:25 +0100

    fix(recurrence): added the custom strategy type to the strategies

--- src/main/java/com/example/SlotlyV2/feature/event/enums/StrategyType.java
+++ src/main/java/com/example/SlotlyV2/feature/event/enums/StrategyType.java
@@ -12,4 +12,8 @@ public class StrategyType {
     public static final String MONTHLY_NEVER = "MONTHLY_NEVER";
     public static final String MONTHLY_OCCURRENCES = "MONTHLY_OCCURRENCES";
     public static final String MONTHLY_DATE = "MONTHLY_DATE";
+
+    public static final String CUSTOM_NEVER = "CUSTOM_NEVER";
+    public static final String CUSTOM_OCCURRENCES = "CUSTOM_OCCURRENCES";
+    public static final String CUSTOM_DATE = "CUSTOM_DATE";
 }
---
+++ src/main/java/com/example/SlotlyV2/feature/event/strategy/CustomDateStrategy.java
@@ -0,0 +1,23 @@
+package com.example.SlotlyV2.feature.event.strategy;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.SlotlyV2.common.util.SlotUtils;
+import com.example.SlotlyV2.feature.event.Event;
+import com.example.SlotlyV2.feature.event.enums.StrategyType;
+import com.example.SlotlyV2.feature.slot.Slot;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service(StrategyType.CUSTOM_DATE)
+public class CustomDateStrategy implements RecurrenceStrategy {
+    private final SlotUtils slotUtils;
+
+    @Override
+    public List<Slot> generateSlots(Event event) {
+        return slotUtils.buildRecurringSlots(event);
+    }
+}
